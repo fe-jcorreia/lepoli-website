6e7c9ee3fcc6c4cc89798cfa91e77a99d169edf7
fe-jcorreia <fe.jcorreia@gmail.com>
2021-02-17 13:22:09 -0300

    Selection process added

--- src/components/SelectionProcess/SelectionProcess.jsx
+++ src/components/SelectionProcess/SelectionProcess.jsx
@@ -7,17 +7,19 @@ function SelectionProcess() {
     <section className="selection-process">
       <h1>Inscreva-se no nosso processo seletivo 2021!</h1>
       <form className="form-signin">
-        <input
-          className="form-signin_input"
-          type="text"
-          placeholder="Digite seu nome completo"
-        />
-        <input
-          className="form-signin_input"
-          type="text"
-          placeholder="Digite seu celular com DDD"
-        />
-        <button className="form-signin_submit" type="submit">
+        <div className="form-signin_inputs">
+          <input
+            className="form-signin_input"
+            type="text"
+            placeholder="Digite seu nome completo"
+          />
+          <input
+            className="form-signin_input"
+            type="text"
+            placeholder="Digite seu celular com DDD"
+          />
+        </div>
+        <button className="form-signin_submit" type="button">
           Prosseguir
         </button>
       </form>
